e77d70ba298c1e5ec85127ffd7ee09fab3759748
Javi Loureiro <javier.loureiro52@gmail.com>
2022-10-19 22:23:11 +0200

    casi terminado

--- Frutas/app/src/main/java/com/example/frutas/MainActivity.java
+++ Frutas/app/src/main/java/com/example/frutas/MainActivity.java
@@ -8,30 +8,29 @@ import android.widget.EditText;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-    Button mostrar = findViewById(R.id.mostrar);
-    ImageView imagen = findViewById(R.id.imagen);
-    EditText opcion = findViewById(R.id.opcion);
-    String input = "";
+    String input;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
+        Button mostrar = findViewById(R.id.mostrar);
+        ImageView imagen = findViewById(R.id.imagen);
+        EditText opcion = findViewById(R.id.opcion);
 
-        input = opcion.getText().toString();
-
-        mostrar.setOnClickListener(view -> {
-            if(input.equals("a")) {
-                imagen.setImageResource(R.drawable.aguacate);
-            }
-            if(input.equals("c")) {
-                imagen.setImageResource(R.drawable.cerezas);
-            }
-            if(input.equals("m")) {
-                imagen.setImageResource(R.drawable.golden);
-            }
-        });
+       mostrar.setOnClickListener(view -> {
+           input = opcion.getText().toString();
+           if(input.equals("a")) {
+               imagen.setImageResource(R.drawable.aguacate);
+           }
+           if(input.equals("c")) {
+               imagen.setImageResource(R.drawable.cerezas);
+           }
+           if(input.equals("m")) {
+               imagen.setImageResource(R.drawable.golden);
+           }
+       });
 
     }
 }
